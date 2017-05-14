eca93e3c85d4f8fa6471c1026617dc67391c89e3
YuriyStr <yuriystr98@gmail.com>
2017-05-14 11:10:35 +0300

    Initial Commit

---
+++ Basket/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Basket
+//
+//  Created by Admin on 14.05.17.
+//  Copyright (c) 2017 Yury Struchkou. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
